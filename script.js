cd4a380146d305e22d801b3d0be95bbd267cae21
fouedd9 <fouedsaidane2@gmail.com>
2022-02-22 13:36:14 +0100

    new questions

--- script.js
+++ script.js
@@ -12,59 +12,74 @@ class Question {
 
 let questions = [
   new Question(
-    "Que retourne Math.min(Math.E , Math.SQRT1_2 , 0.5) ?",
-    ["indexOf()", "map()", "filter()", "reduce()"],
-    "filter()"
-  ),
-  new Question(
-    "Quelles méthode Javascript permet de filtrer les élements d'un tableau ?",
-    ["indexOf()", "map()", "filter()", "reduce()"],
-    "filter()"
-  ),
-  new Question(
-    "Comment afficher 'Bonjour' sur la console ?",
+    "Le gardien lance le ballon devant lui pour le dégager au pied, de volée. Alors que le ballon est en lair, un attaquant le lui subtilise et marque. Décisions  ?",
     [
-      "log('Bonjour')",
-      "prompt('Bonjour')",
-      "console.log(Bonjour')",
-      "console('Bonjour')",
+      "But accordé. Coup d’envoi.",
+      "But refusé. Coup franc indirect pour la défense",
+      "But refusé. Avertissement. Coup franc indirect pour la défense",
+      "But refusé. Coup franc direct pour la défense",
     ],
-    "console.log(Bonjour')"
+    "But refusé. Coup franc indirect pour la défense"
   ),
   new Question(
-    "Comment afficher 'Bonjour' dans une boite de dialogue ?",
+    "Le tireur d’un penalty marque un temps d’arrêt dans sa course d’élan, la reprend surquelques foulées, tire et marque, Décisions  ?",
     [
-      "print('bonjour');",
-      "write('bonjour')",
-      "alert('bonjour')",
-      "echo('bonjour');",
+      "But accordé.",
+      "But refusé. Penalty à retirer",
+      "But refusé. Penalty à retirer",
+      "penalty a refaire",
     ],
-    "alert('bonjour')"
+    "But accordé."
   ),
   new Question(
-    "Comment lire le jour du mois dans une date ?",
-    ["Date.parse()", "getDate()", "getDay()", "Day()"],
-    "getDate()"
-  ),
-  new Question(
-    "Que renvoie la fonction T1.map(MaFonction) ?",
+    "Rentrée de touche A. Un adversaire B placé à un mètre dévie le ballon pour un de ses partenaires.Décisions ?",
     [
-      "un nouveau tableau d'éléments traités par 'MaFonction' ",
-      "le même tableau T1 où 'MaFonction' s'est appliqué à tous les éléments",
-      "une chaîne d'éléments traités par 'MaFonction'",
-      "une chaîne d'éléments traités par 'MaZebbi'",
+      "Laisser jouer",
+      "Rentrée de touche à refaire pour A.",
+      "Avertissement.Coup franc indirect A à l’endroit de B.",
+      "Coup franc indirect A à l’endroit de B sans avertissement",
     ],
-    "un nouveau tableau d'éléments traités par 'MaFonction' "
+    "Rentrée de touche à refaire pour A."
   ),
   new Question(
-    "Que renvoie la fonction T1.map(MaFonction) ?",
+    "Coup franc indirect pour l’attaque. Le tireur frappe au but. Le gardien plonge, touche le ballon, mais ne l’empêche pas d’entrer dans ses buts.Décisions ?",
     [
-      "un nouveau tableau d'éléments traités par 'MaFonction' ",
-      "le même tableau T1 où 'MaFonction' s'est appliqué à tous les éléments",
-      "NON",
-      "une chaîne d'éléments traités par 'MaZebbi'",
+      "But accordé.;",
+      "But refusé. CFI à retirer.",
+      "But refusé. Corner.",
+      "CFI à refaire;",
     ],
-    "NON"
+    "But accordé."
+  ),
+  new Question(
+    "Penalty. Deux partenaires du tireur entrent nettement dans la surface de réparation avant le tir. Le ballon est dévié en corner par le gardien.Décisions  ?",
+    [
+      "Penalty à retirer.",
+      "Corner",
+      "Coup franc indirect pour la défense.",
+      "Coup franc direct pour la défense.",
+    ],
+    "Coup franc indirect pour la défense."
+  ),
+  new Question(
+    "Epreuve des tirs au but. Le capitaine A gagne le toss.Qui commence à tirer ?",
+    [
+      "L’équipe A obligatoirement. ",
+      "L’équipe B obligatoirement.",
+      "A ou B selon le choix du capitaine A",
+      "A ou B selon le choix du capitaine B",
+    ],
+    "A ou B selon le choix du capitaine A "
+  ),
+  new Question(
+    "Un attaquant crie « laisse ! » à un partenaire moins bien placé que lui, reprend le ballon et marque.Décisions ?",
+    [
+      "But accordé.",
+      "But refusé. Coup franc indirect pour la défense.",
+      "But refusé. Coup franc direct pour la défense.",
+      "Coup franc indirect dans la surface de réparation ",
+    ],
+    "But accordé."
   ),
 ];
 // console.log(questions);
